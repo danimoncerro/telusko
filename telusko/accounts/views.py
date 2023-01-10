a7cbfab9f220c92aaaa32973ccf7cc8a92d73678
daniel <dani_moncerro@yahoo.com>
2023-01-11 00:31:02 +0200

    django tutorial 25

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -48,3 +48,7 @@ def register(request):
         return redirect('/')
     else:
         return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
