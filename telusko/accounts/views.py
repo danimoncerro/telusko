b3d38f3ad7f2813f151b06c0b9feed561d931d61
daniel <dani_moncerro@yahoo.com>
2023-01-10 22:57:55 +0200

    django tutorial 23

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,16 +15,19 @@ def register(request):
 
         if password1==password2:
             if User.objects.filter(username=username).exists():
-                print('Username taken')
+                messages.info(request, 'Username taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('email taken')
+                messages.info(request, 'Email taken')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
 
         else:
-            print('password not matching...')
+            messages.info(request, 'password not matching...')
+            return redirect('register')
         return redirect('/')
     else:
         return render(request, 'register.html')
